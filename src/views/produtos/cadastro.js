421a257fd5706b1181c2ff0d7df30e8cf432368c
welliton <josealves_w@yahoo.com.br>
2020-03-12 01:59:28 -0300

    layout cadastro page

--- src/views/produtos/cadastro.js
+++ src/views/produtos/cadastro.js
@@ -9,6 +9,57 @@ export default class CadastroProduto extends React.Component{
                 </div>
                 <div className="card-body">
 
+                    <div className="row">
+                        <div className="col-md-6">
+                            <div className="form-group">
+                                <label>Nome:*</label>
+                                <input type="text" className="form-control"/>
+                            </div>
+                        </div>
+
+                        <div className="col-md-6">
+                            <div className="form-group">
+                                <label>SKU:*</label>
+                                <input type="text" className="form-control"/>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="form-group">
+                            <label>Descrição:</label>
+                            <textarea className="form-control"/>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-md-6">
+                            <div className="form-group">
+                                <label>Preço:*</label>
+                                <input type="text" className="form-control"/>
+                            </div>
+                        </div>
+
+                        <div className="col-md-6">
+                            <div className="form-group">
+                                <label>Fornecedor:*</label>
+                                <input type="text" className="form-control"/>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-md-1">
+                            <button className="btn btn-success">Salvar</button>
+                        </div>
+                        <div className="col-md-1">
+                            <button className="btn btn-primary">Limpar</button>
+                        </div>
+                    </div>
+
                 </div>
             </div>
 
