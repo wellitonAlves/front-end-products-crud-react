c964c33e1e41f1608f6e3259086b556a7f56e5f0
welliton <josealves_w@yahoo.com.br>
2020-03-11 23:54:54 -0300

    create view home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import Navbar from './components/navbar';
+import Home from './views/home'
 
 function App() {
   return (
-    <div className="alert alert-dismissible alert-warning">
-      <button type="button" class="close" data-dismiss="alert">&times;</button>
-      <h4 className="alert-heading">Warning!</h4>
-      <p className="mb-0">Best check yo self, you're not looking too good. Nulla vitae elit libero, a pharetra augue. Praesent commodo cursus magna, <a href="#" class="alert-link">vel scelerisque nisl consectetur et</a>.</p>
-    </div>
+      <>
+        <div className="container">
+          <Navbar />
+          <Home />
+        </div>
+      </>
   );
 }
 
---
+++ src/views/produts/cadastro.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default class CadastroProduto extends React.Component{
+    
+}
